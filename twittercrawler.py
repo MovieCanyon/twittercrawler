8692418c29a168ed1216f4453bd0816f9232b675
Asad Mahmood <2325036m@student.gla.ac.uk>
2020-03-14 18:03:31 +0000

    Making Change

--- twittercrawler.py
+++ twittercrawler.py
@@ -8,10 +8,10 @@ with open('twitterdata.csv', 'a') as file:
     writer.writerow(["username", "tweet"])
 
 # keys
-consumer_key = #consumer_key goes here
-consumer_secret = #consumer_secret goes here
-access_token = #access_token goes here
-access_token_secret = #access_token_secret goes here
+consumer_key = 'consumer_key goes here'
+consumer_secret = 'consumer_secret goes here'
+access_token = 'access_token goes here'
+access_token_secret = 'access_token_secret goes here'
 
 twitterREST.crawl(consumer_key, consumer_secret, access_token, access_token_secret)
 twitterSTREAM.crawl(consumer_key, consumer_secret, access_token, access_token_secret)
